88eb69dc4056aa68ec122cb7b787d8c78a59a8ca
Jagdip1 <46296807+Jagdip1@users.noreply.github.com>
2019-01-01 19:57:04 +0530

    hello

---
+++ hhh/src/hhh/H.java
@@ -0,0 +1,30 @@
+package hhh;
+import java.sql.*;
+
+public class H {
+	
+	public static void main(String[] args) throws Exception {
+	System.out.println("hello");
+
+	
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	
+	try {
+		
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/jaggi?autoReconnect=true&useSSL=false", "root", "raftaar");
+		if(c!=null) {System.out.println("yes");}
+		
+		Statement t =c.createStatement();
+	    ResultSet s = t.executeQuery("select * from jaggu where 1 ");
+	   while(s.next()) {
+	   System.out.println(s.getInt(1));
+	   System.out.println(s.getString(2));}
+	    		
+		
+	}
+catch(Exception e ) {
+	
+	System.out.println(e);
+}
+}
+}
